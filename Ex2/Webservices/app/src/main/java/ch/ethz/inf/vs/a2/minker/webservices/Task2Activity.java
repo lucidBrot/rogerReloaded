ec76a85096ffda08fad1ea407d5372f4e2bf2d5e
Eric Mink <eric@mink.li>
2017-10-17 20:34:39 +0200

    started implementing XmlSensor
    mostly by writing code that I found on stackoverflow, but that's the nature of this assignment. App is currently compiling - because nothing is called yet.
    TODO:
    	find out how the parsing is called
    	pass the XML SOAP request data to the HTTP client

--- Ex2/Webservices/app/src/main/java/ch/ethz/inf/vs/a2/minker/webservices/Task2Activity.java
+++ Ex2/Webservices/app/src/main/java/ch/ethz/inf/vs/a2/minker/webservices/Task2Activity.java
@@ -3,11 +3,32 @@ package ch.ethz.inf.vs.a2.minker.webservices;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-public class Task2Activity extends AppCompatActivity {
+public class Task2Activity extends AppCompatActivity implements ch.ethz.inf.vs.a2.sensor.SensorListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task2);
+    }
+
+    /**
+     * Callback function for receiving sensor values.
+     *
+     * @param value A sensor value
+     */
+    @Override
+    public void onReceiveSensorValue(double value) {
+
+    }
+
+    /**
+     * Callback function for receiving messages.
+     * Could be useful for debugging.
+     *
+     * @param message Message
+     */
+    @Override
+    public void onReceiveMessage(String message) {
+
     }
 }
