df18fc21d8d2bbbd27282a240a7fcc5ec69e9708
Eric Mink <eric@mink.li>
2017-10-18 14:03:24 +0200

    Manual Part of Task2 is running
    Please excuse the hard-coded address and requests used in the XmlSensor.
    The interface made it hard to make this more dynamic.

--- Ex2/Webservices/app/src/main/java/ch/ethz/inf/vs/a2/minker/Task2Activity.java
+++ Ex2/Webservices/app/src/main/java/ch/ethz/inf/vs/a2/minker/Task2Activity.java
@@ -2,13 +2,23 @@ package ch.ethz.inf.vs.a2.minker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Task2Activity extends AppCompatActivity implements ch.ethz.inf.vs.a2.minker.sensor.SensorListener {
 
+    private XmlSensor xmlSensor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task2);
+
+        xmlSensor = new XmlSensor();
+        xmlSensor.registerListener(this);
+
+        xmlSensor.getTemperature();
+
     }
 
     /**
@@ -18,7 +28,9 @@ public class Task2Activity extends AppCompatActivity implements ch.ethz.inf.vs.a
      */
     @Override
     public void onReceiveSensorValue(double value) {
-
+        Log.d("Task2/Activity", "received sensor value: "+value);
+        String s = getString(R.string.t2_temperature) + String.valueOf(value);
+        ((TextView) findViewById(R.id.t2_temperature_display)).setText(s);
     }
 
     /**
--- Ex2/Webservices/app/src/main/java/ch/ethz/inf/vs/a2/minker/XmlSensor.java
+++ Ex2/Webservices/app/src/main/java/ch/ethz/inf/vs/a2/minker/XmlSensor.java
@@ -1,12 +1,19 @@
 package ch.ethz.inf.vs.a2.minker;
 
+import android.support.annotation.Nullable;
 import android.util.Log;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import static java.net.Proxy.Type.HTTP;
 
 public class XmlSensor extends ch.ethz.inf.vs.a2.minker.sensor.AbstractSensor {
 
@@ -24,6 +31,7 @@ public class XmlSensor extends ch.ethz.inf.vs.a2.minker.sensor.AbstractSensor {
         this.serviceAddress = serviceAddress;
         this.serviceAction = serviceAction;
     }
+
     /**
      * Executes a request to fetch the temperature value.
      *
@@ -40,10 +48,11 @@ public class XmlSensor extends ch.ethz.inf.vs.a2.minker.sensor.AbstractSensor {
         try{
             URL urlObj = new URL(serviceAddress);
             conn = (HttpURLConnection) urlObj.openConnection();
+            conn.setRequestProperty("Content-Type", "text/xml; charset=utf-8");
             conn.setRequestMethod("POST");
             conn.setDoOutput(true); // as recommended by the exercise
             OutputStreamWriter вр = new OutputStreamWriter(conn.getOutputStream());
-            String data = ""; //TODO: load data
+            String data = buildRequest("getSpot3"); // TODO: Actually get temperature
             // possibly helpful links:
             /*
             https://stackoverflow.com/a/2560129/2550406 <--- building XML into string
@@ -58,7 +67,7 @@ public class XmlSensor extends ch.ethz.inf.vs.a2.minker.sensor.AbstractSensor {
 
             вр.write(data);
             вр.flush();
-            Log.d("executeRequest", "post response code: " + conn.getResponseCode());
+            Log.d("Task2/executeRequest", "post response code: " + conn.getResponseCode());
 
             reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             StringBuilder stringBuilder = new StringBuilder();
@@ -69,7 +78,8 @@ public class XmlSensor extends ch.ethz.inf.vs.a2.minker.sensor.AbstractSensor {
 
             response = stringBuilder.toString();
         } catch (Exception e){
-            Log.e("executeRequest", "Caught Exception : "+e.getMessage());
+            Log.e("Task2/executeRequest", "Caught Exception : "+e.getMessage());
+            e.printStackTrace();
             throw(e);
         } finally {
             try{
@@ -80,11 +90,57 @@ public class XmlSensor extends ch.ethz.inf.vs.a2.minker.sensor.AbstractSensor {
                     conn.disconnect();
                 }
             } catch (Exception e){
-                Log.e("executeRequest", "Caught YET ANOTHER Exception");
+                Log.e("Task2/executeRequest", "Caught YET ANOTHER Exception");
                 throw(e);
             }
         }
         return response;
+    }
+
+
+    /**
+     * @param name can be <i>getDiscoveredSpots</i>
+     * @return an XML Request or null
+     */
+    @Nullable
+    private String buildRequest(String name) {
+        StringBuilder sb = new StringBuilder();
+        switch(name){
+            case "getDiscoveredSpots":
+            {
+                sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?><S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\">");
+                sb.append("<S:Header/>");
+                sb.append("<S:Body>");
+                sb.append("<ns2:getDiscoveredSpots xmlns:ns2=\"http://webservices.vslecture.vs.inf.ethz.ch/\"/>");
+                sb.append("</S:Body>");
+                sb.append("</S:Envelope>");
+                return sb.toString();
+            }
+            case "getSpot3":
+            {
+                return
+                        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\">"
+                                + "<S:Header/><S:Body>"
+                                +"<ns2:getSpot xmlns:ns2=\"http://webservices.vslecture.vs.inf.ethz.ch/\">"
+                                +"<id>Spot3</id>"
+                                +"</ns2:getSpot>"
+                                +"</S:Body>"
+                                +"</S:Envelope>";
+            }
+            case "getSpot4":
+            {
+                return
+                        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\">"
+                                + "<S:Header/><S:Body>"
+                                +"<ns2:getSpot xmlns:ns2=\"http://webservices.vslecture.vs.inf.ethz.ch/\">"
+                                +"<id>Spot4</id>"
+                                +"</ns2:getSpot>"
+                                +"</S:Body>"
+                                +"</S:Envelope>";
+            }
+            default:
+                return null;
+        }
     }
 
     /**
@@ -95,7 +151,61 @@ public class XmlSensor extends ch.ethz.inf.vs.a2.minker.sensor.AbstractSensor {
      */
     @Override
     public double parseResponse(String response) {
-        return 0;
+
+        Log.d("Task2/executeRequest", "response was "+response);
+
+        XmlPullParserFactory factory = null;
+        try {
+            factory = XmlPullParserFactory.newInstance();
+        } catch (XmlPullParserException e) {
+            Log.e("Task2/executeRequest", "Failed to instantiate XmlPullParserFactory");
+            e.printStackTrace();
+            return Double.NaN;
+        }
+        factory.setNamespaceAware(true);
+        XmlPullParser xpp = null;
+        try {
+            xpp = factory.newPullParser();
+        } catch (XmlPullParserException e) {
+            Log.e("Task2/executeRequest", "Failed to instantiate PullParser");
+            e.printStackTrace();
+            return Double.NaN;
+        }
+
+        try {
+            xpp.setInput( new StringReader( response ) );
+        } catch (XmlPullParserException e) {
+            Log.e("Task2/executeRequest", "failed to set Input for Parser");
+            e.printStackTrace();
+            return Double.NaN;
+        }
+        int eventType = 0;
+        double temperature;
+        try {
+            eventType = xpp.getEventType();
+
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                if(eventType == XmlPullParser.START_DOCUMENT) {
+                    Log.d("Task2/Parser","Start document");
+                } else if(eventType == XmlPullParser.START_TAG) {
+                    Log.d("Task2/Parser", "Start tag "+xpp.getName());
+                    if (xpp.getName().equals("temperature")){
+                        temperature = Double.parseDouble(xpp.nextText());
+                        return temperature;
+                    }
+                } else if(eventType == XmlPullParser.END_TAG) {
+                    Log.d("Task2/Parser", "End tag "+xpp.getName());
+                } else if(eventType == XmlPullParser.TEXT) {
+                    Log.d("Task2/Parser", "Text "+xpp.getText());
+                }
+                eventType = xpp.next();
+            }
+        } catch (Exception e) {
+            Log.e("Task2/executeRequest", "Failed attempt of Parsing");
+            e.printStackTrace();
+            return Double.NaN;
+        }
+        return Double.NaN;
     }
 
 }
