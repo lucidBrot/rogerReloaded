e917a04b9c86e6efff4d7ada1e59e7c310bb81f0
Eric Mink <eric@mink.li>
2017-10-25 14:51:50 +0200

    some javadoc

--- Ex3/VSminkerChat/app/src/main/java/ch/ethz/inf/vs/a3/vsminkerchat/ChatActivity.java
+++ Ex3/VSminkerChat/app/src/main/java/ch/ethz/inf/vs/a3/vsminkerchat/ChatActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+/**
+ * Should be started when Task1Registrator returns true
+ */
 public class ChatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
